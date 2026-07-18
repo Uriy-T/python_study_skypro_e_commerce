4afac669887f66ce0e8875f6ca506370bf75d88e
Uriy-T <briner_asterios@mail.ru>
2026-07-18 13:10:04 +0400

    Добавил фикстуру создания продукта с помощью класс-метода.

--- fixtures/entities_fixtures.py
+++ fixtures/entities_fixtures.py
@@ -20,6 +20,21 @@ def create_product_valid(request: FixtureRequest) -> Product:
     name, description, price, quantity = request.param
     return Product(name, description, price, quantity)
 
+@pytest.fixture(
+    scope="function",
+    params=[
+        (
+            "Samsung Galaxy S 200",
+            "Лучшее устройство для лучшей связи",
+            57000,
+            23,
+        )
+    ],
+)
+def create_product_by_classmethod(request: FixtureRequest) -> Product:
+    params = dict(zip(['name', 'description', 'price', 'quantity'], request.param))
+    return Product.new_product(params)
+
 
 @pytest.fixture(scope="function")
 def create_category(create_product_valid: Product) -> Category:
