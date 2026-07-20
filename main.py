5c0b33ed7fd13d4dd6ebc9fcc41bedaf3c2badc3
Uriy-T <briner_asterios@mail.ru>
2026-07-20 13:39:10 +0400

    Заменил способ обращения к списку продуктов с параметра на свойство get_products .

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     print(str(category1))
 
-    print(category1.products)
+    print(category1.get_products)
 
     print(product1 + product2)
     print(product1 + product3)
