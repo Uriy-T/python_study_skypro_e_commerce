497ccb5614a013c889690248cadb0f14f9631f49
Uriy-T <briner_asterios@mail.ru>
2026-07-20 22:24:25 +0400

    Добавил набор тестовых данных для негативных тестов сложения объекта типа Product с объектами других типов.

--- data_for_tests/product_test_data/test_datasets.py
+++ data_for_tests/product_test_data/test_datasets.py
@@ -42,7 +42,7 @@ product_create_invalid_data: list[dict[str, Any]] = [
         },
         "exception_type": TypeError,
         "system_answer": "Product.__init__() missing 1"
-        " required positional argument: 'name'",
+                         " required positional argument: 'name'",
     },
     {
         "data_for_create": {
@@ -52,7 +52,7 @@ product_create_invalid_data: list[dict[str, Any]] = [
         },
         "exception_type": TypeError,
         "system_answer": "Product.__init__() missing 1"
-        " required positional argument: 'description'",
+                         " required positional argument: 'description'",
     },
     {
         "data_for_create": {
@@ -62,7 +62,7 @@ product_create_invalid_data: list[dict[str, Any]] = [
         },
         "exception_type": TypeError,
         "system_answer": "Product.__init__() missing 1"
-        " required positional argument: 'price'",
+                         " required positional argument: 'price'",
     },
     {
         "data_for_create": {
@@ -72,7 +72,7 @@ product_create_invalid_data: list[dict[str, Any]] = [
         },
         "exception_type": TypeError,
         "system_answer": "Product.__init__() missing 1"
-        " required positional argument: 'quantity'",
+                         " required positional argument: 'quantity'",
     },
 ]
 
@@ -447,4 +447,44 @@ set_price_with_incorrect_type: list[dict[str, Any]] = [
     {"new_price": {1, 2, 3}},
     {"new_price": (1, 2, 3)},
     {"new_price": {"test_price": 1}},
+]
+
+add_incorrect_objects: list[dict[str, Any]] = [
+    {
+        "incorrect_object": 1,
+        "expected_answer": "'int' object has no attribute 'price'"
+    },
+
+    {
+        "incorrect_object": 1.1,
+        "expected_answer": "'float' object has no attribute 'price'"
+    },
+
+    {
+        "incorrect_object": '1',
+        "expected_answer": "'str' object has no attribute 'price'"
+    },
+
+    {
+        "incorrect_object": False,
+        "expected_answer": "'bool' object has no attribute 'price'"
+    },
+    {
+        "incorrect_object": [1, 2, 3],
+        "expected_answer": "'list' object has no attribute 'price'"
+    },
+    {
+        "incorrect_object": (1, 2, 3),
+        "expected_answer": "'tuple' object has no attribute 'price'"
+    },
+
+    {
+        "incorrect_object": {1, 2, 3},
+        "expected_answer": "'set' object has no attribute 'price'"
+    },
+
+    {
+        "incorrect_object": {"object": 1},
+        "expected_answer": "'dict' object has no attribute 'price'"
+    },
 ]
