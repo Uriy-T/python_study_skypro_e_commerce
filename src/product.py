d5c76f4b8c6e16b4c1f8ba71254c8190fe24a3d7
Uriy-T <briner_asterios@mail.ru>
2026-07-23 11:53:49 +0400

    Доработки: -- Реализовал субклассы Product: * Smartphone; * LawnGrass.
    -- Добавил для метода __add__ валидацию типа для объекта сложения.

--- src/product.py
+++ src/product.py
@@ -64,4 +64,41 @@ class Product:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other: "Product") -> int | float:
+        if not isinstance(other, Product):
+            raise TypeError
         return (self.price * self.quantity) + (other.price * other.quantity)
+
+
+class Smartphone(Product):
+
+    def __init__(self, name: str,
+                 description: str,
+                 price: float,
+                 quantity: int,
+                 efficiency: float,
+                 model: str,
+                 memory: int,
+                 color: str):
+        super().__init__(name,
+                         description,
+                         price,
+                         quantity)
+        self.efficiency = efficiency
+        self.model = model,
+        self.memory = memory,
+        self.color = color
+
+class LawnGrass(Product):
+
+    def __init__(self,
+                 name: str,
+                 description: str,
+                 price: float,
+                 quantity: int,
+                 country: str,
+                 germination_period: str,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period,
+        self.color = color
